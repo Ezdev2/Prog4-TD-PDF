88ee37d7b07d409fa073f4650ebc678d55c8b60f
ezdev2 <fansoaezra@gmail.com>
2023-08-27 22:32:27 +0300

    first commit

---
+++ src/main/java/com/example/prog4swa/PROG4SWAApplication.java
@@ -0,0 +1,61 @@
+package com.example.prog4swa;
+
+import com.example.prog4swa.db1.controller.security.SessionFilter;
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+@SpringBootApplication
+@Configuration
+public class PROG4SWAApplication implements WebMvcConfigurer{
+
+	@Autowired
+	private SessionFilter sessionFilter;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(sessionFilter).addPathPatterns("/**");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("/webjars/")
+				.resourceChain(false);
+	}
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		UrlPathHelper urlPathHelper = new UrlPathHelper();
+		urlPathHelper.setRemoveSemicolonContent(false);
+		configurer.setUrlPathHelper(urlPathHelper);
+	}
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+		filter.setMethodParam("_method");
+		return filter;
+	}
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(DataSize.parse("10MB"));
+		factory.setMaxRequestSize(DataSize.parse("10MB"));
+		return factory.createMultipartConfig();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PROG4SWAApplication.class, args);
+	}
+
+}
